5a85e861adf09e8283ed4b7d102eeef28ab0f26d
LaXHeXLuX <laas.lumberg@gmail.com>
2024-05-14 10:21:35 +0300

    Fraction MVP complete

--- src/Fraction.java
+++ src/Fraction.java
@@ -5,8 +5,8 @@ public class Fraction {
     public Fraction(long numerator, long denominator) {
         if (denominator == 0) throw new IllegalArgumentException("Fraction: denominator can't be zero!");
         if (denominator < 0) {
-            denominator *= -1;
-            numerator *= -1;
+            denominator = -denominator;
+            numerator = -numerator;
         }
         this.numerator = numerator;
         this.denominator = denominator;
@@ -19,5 +19,25 @@ public class Fraction {
     public Fraction simplify() {
         long gcd = Divisors.greatestCommonDivisor(numerator, denominator);
         return new Fraction(numerator / gcd, denominator / gcd);
+    }
+
+    public Fraction add(Fraction fraction) {
+        long gcd = Divisors.greatestCommonDivisor(denominator, fraction.denominator);
+        long newDenominator = denominator * fraction.denominator / gcd;
+        long newNumerator1 = numerator * (newDenominator/denominator);
+        long newNumerator2 = fraction.numerator * (newDenominator/fraction.denominator);
+        return new Fraction(newNumerator1 + newNumerator2, newDenominator);
+    }
+
+    public Fraction subtract(Fraction fraction) {
+        return this.add(new Fraction(-fraction.numerator, fraction.denominator));
+    }
+
+    public Fraction multiply(Fraction fraction) {
+        return new Fraction(numerator * fraction.numerator, denominator * fraction.denominator);
+    }
+
+    public Fraction divide(Fraction fraction) {
+        return this.multiply(new Fraction(fraction.denominator, fraction.numerator));
     }
 }
--- src/test/FractionTest.java
+++ src/test/FractionTest.java
@@ -6,6 +6,11 @@ class FractionTest {
     void assertFractionEquals(long[] fractionAsArray, Fraction fraction) {
         assertArrayEquals(fractionAsArray, fraction.asArray());
     }
+
+    void assertSimplifiedFractionEquals(long[] fractionAsArray, Fraction fraction) {
+        assertArrayEquals(fractionAsArray, fraction.simplify().asArray());
+    }
+
     @Test
     void createFraction() {
         assertThrows(IllegalArgumentException.class, () -> new Fraction(0, 0));
@@ -20,5 +25,84 @@ class FractionTest {
 
         fraction = new Fraction(2, 2);
         assertFractionEquals(new long[] {1, 1}, fraction.simplify());
+
+        fraction = new Fraction(26, 8);
+        assertFractionEquals(new long[] {13, 4}, fraction.simplify());
+    }
+
+    @Test
+    void addFraction() {
+        Fraction fraction1 = new Fraction(1, 1);
+        Fraction fraction2 = new Fraction(0, 1);
+        Fraction sum = fraction1.add(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 1}, sum);
+
+        fraction1 = new Fraction(1, 1);
+        fraction2 = new Fraction(1, 1);
+        sum = fraction1.add(fraction2);
+        assertSimplifiedFractionEquals(new long[] {2, 1}, sum);
+
+        fraction1 = new Fraction(1, 2);
+        fraction2 = new Fraction(1, 3);
+        sum = fraction1.add(fraction2);
+        assertSimplifiedFractionEquals(new long[] {5, 6}, sum);
+    }
+
+    @Test
+    void subtractFraction() {
+        Fraction fraction1 = new Fraction(1, 1);
+        Fraction fraction2 = new Fraction(0, 1);
+        Fraction difference = fraction1.subtract(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 1}, difference);
+
+        fraction1 = new Fraction(0, 1);
+        fraction2 = new Fraction(1, 1);
+        difference = fraction1.subtract(fraction2);
+        assertSimplifiedFractionEquals(new long[] {-1, 1}, difference);
+
+        fraction1 = new Fraction(1, 2);
+        fraction2 = new Fraction(1, 3);
+        difference = fraction1.subtract(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 6}, difference);
+    }
+
+    @Test
+    void multiplyFraction() {
+        Fraction fraction1 = new Fraction(1, 1);
+        Fraction fraction2 = new Fraction(0, 1);
+        Fraction product = fraction1.multiply(fraction2);
+        assertSimplifiedFractionEquals(new long[] {0, 1}, product);
+
+        fraction1 = new Fraction(0, 1);
+        fraction2 = new Fraction(1, 1);
+        product = fraction1.multiply(fraction2);
+        assertSimplifiedFractionEquals(new long[] {0, 1}, product);
+
+        fraction1 = new Fraction(1, 3);
+        fraction2 = new Fraction(3, 1);
+        product = fraction1.multiply(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 1}, product);
+
+        fraction1 = new Fraction(1, 2);
+        fraction2 = new Fraction(1, 3);
+        product = fraction1.multiply(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 6}, product);
+    }
+
+    @Test
+    void divideFraction() {
+        Fraction dividend = new Fraction(1, 1);
+        Fraction divider = new Fraction(0, 1);
+        assertThrows(IllegalArgumentException.class, () -> dividend.divide(divider));
+
+        Fraction fraction1 = new Fraction(1, 3);
+        Fraction fraction2 = new Fraction(3, 1);
+        Fraction quotient = fraction1.divide(fraction2);
+        assertSimplifiedFractionEquals(new long[] {1, 9}, quotient);
+
+        fraction1 = new Fraction(1, 2);
+        fraction2 = new Fraction(1, 3);
+        quotient = fraction1.divide(fraction2);
+        assertSimplifiedFractionEquals(new long[] {3, 2}, quotient);
     }
 }
