c83ba5d2281351e1bc893263543ceeb9e5159921
LaXHeXLuX <laas.lumberg@gmail.com>
2024-06-04 14:01:39 +0300

    Added Parser class and tests

---
+++ src/Parser.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    public static String[] parseStringRows(String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line = br.readLine();
+
+        if (line == null) {
+            br.close();
+            return new String[0];
+        }
+
+        List<String> rows = new ArrayList<>();
+
+        while (line != null) {
+            rows.add(line);
+            line = br.readLine();
+        }
+
+        br.close();
+        return Converter.listToArr(rows);
+    }
+    public static int[] parseInts(String filename) throws IOException {
+        String[] rows = parseStringRows(filename);
+        int[] ints = new int[rows.length];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = Integer.parseInt(rows[i]);
+        }
+
+        return ints;
+    }
+}
---
+++ src/test/ParserTest.java
@@ -0,0 +1,66 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.TestAbortedException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    String filename = "parse.txt";
+
+    @BeforeEach
+    public void createFile() throws IOException {
+        File file = new File(filename);
+        if (!file.createNewFile()) throw new TestAbortedException("File " + file.getName() + " already exists!");
+    }
+    @AfterEach
+    public void deleteFile() {
+        File file = new File(filename);
+        if (!file.delete()) throw new TestAbortedException("File " + file.getName() + " doesn't exist!");
+    }
+    @Test
+    void parseStringRows() throws IOException {
+        assertArrayEquals(new String[] {}, Parser.parseStringRows(filename));
+
+        FileWriter fw1 = new FileWriter(filename);
+        fw1.write("a");
+        fw1.close();
+        assertArrayEquals(new String[] {"a"}, Parser.parseStringRows(filename));
+
+        FileWriter fw2 = new FileWriter(filename);
+        fw2.write("\n");
+        fw2.close();
+        assertArrayEquals(new String[] {""}, Parser.parseStringRows(filename));
+
+        FileWriter fw3 = new FileWriter(filename);
+        fw3.write("a\nb");
+        fw3.close();
+        assertArrayEquals(new String[] {"a", "b"}, Parser.parseStringRows(filename));
+
+        FileWriter fw4 = new FileWriter(filename);
+        fw4.write("a very\ncool brown\ndog");
+        fw4.close();
+        assertArrayEquals(new String[] {"a very", "cool brown", "dog"}, Parser.parseStringRows(filename));
+    }
+
+    @Test
+    void parseInts() throws IOException {
+        assertArrayEquals(new int[] {}, Parser.parseInts(filename));
+
+        FileWriter fw1 = new FileWriter(filename);
+        fw1.write("1");
+        fw1.close();
+        assertArrayEquals(new int[] {1}, Parser.parseInts(filename));
+
+        FileWriter fw2 = new FileWriter(filename);
+        fw2.write("123\n456");
+        fw2.close();
+        assertArrayEquals(new int[] {123, 456}, Parser.parseInts(filename));
+    }
+}
