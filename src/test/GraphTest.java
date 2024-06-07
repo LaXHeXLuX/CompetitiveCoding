2d262ae87cc43c4e087ed6b7b1bb03df7b2af342
LaXHeXLuX <laas.lumberg@gmail.com>
2024-06-07 19:43:37 +0300

    Added initial Graph

---
+++ src/test/GraphTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+    Graph[] graphs = new Graph[4];
+    @BeforeEach
+    void makeGraphs() {
+        graphs[0] = new Graph(0);
+        graphs[1] = new Graph(1);
+        int[][] edgeMatrix2 = {{-1, 1}, {1, -1}};
+        graphs[2] = new Graph(edgeMatrix2);
+        int[][] edgeMatrix3 = {{-1, -1, 1}, {-1, -1, -1}, {1, 1, -1}};
+        graphs[3] = new Graph(edgeMatrix3);
+    }
+    @Test
+    void nodeCount() {
+        int[] nodeCount = {0, 1, 2, 3};
+        for (int i = 0; i < graphs.length; i++) {
+            assertEquals(nodeCount[i], graphs[i].nodeCount());
+        }
+    }
+    @Test
+    void outgoingEdges() {
+        int[] outgoingEdges = {0, 1, 2};
+        for (int i = 1; i < graphs.length; i++) {
+            assertEquals(outgoingEdges[i-1], graphs[i].outgoingEdges(String.valueOf(i-1)).size());
+        }
+    }
+    @Test
+    void incomingEdges() {
+        int[] incomingEdges = {0, 1, 1};
+        for (int i = 1; i < graphs.length; i++) {
+            assertEquals(incomingEdges[i-1], graphs[i].incomingEdges(String.valueOf(i-1)).size());
+        }
+    }
+    @Test
+    void djikstra() {
+        assertEquals(1, graphs[2].djikstra("0", "1"));
+        assertEquals(2, graphs[3].djikstra("0", "1"));
+        assertEquals(-1, graphs[3].djikstra("1", "0"));
+
+        int[][] edgeMatrix = {{-1, 1, -1, -1, -1}, {-1, -1, 3, -1, -1}, {5, -1, -1, -1, 1}, {-1, -1, -1, -1, -1}, {2, 7, -1, -1, -1}};
+        Graph bigGraph = new Graph(edgeMatrix);
+        assertEquals(0, bigGraph.djikstra("0", "0"));
+        assertEquals(4, bigGraph.djikstra("0", "2"));
+        assertEquals(6, bigGraph.djikstra("1", "0"));
+        assertEquals(-1, bigGraph.djikstra("3", "0"));
+        assertEquals(3, bigGraph.djikstra("4", "1"));
+    }
+}
