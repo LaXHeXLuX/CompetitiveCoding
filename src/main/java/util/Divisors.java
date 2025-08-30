c4eb665d277b7652134efda65712fae7eb90ab72
LaXHeXLuX <laas.lumberg@gmail.com>
2025-08-30 07:20:24 +0300

    Refactoring to convention, use gradle, add tests for 100% BC

---
+++ src/main/java/util/Divisors.java
@@ -0,0 +1,64 @@
+package util;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Divisors {
+    public static long[] divisors(long n) {
+        if (n < 1) throw new RuntimeException("Argument must be positive!");
+        if (n == 1) return new long[] {1};
+        List<Long> divisors = new ArrayList<>();
+        divisors.add(1L);
+        List<Long> biggerDivisors = new ArrayList<>();
+
+        for (long i = 2; i <= Math.pow(n, 0.5); i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+                long counterPart = n/i;
+                if (counterPart != i) biggerDivisors.add(counterPart);
+            }
+        }
+
+        for (int i = biggerDivisors.size()-1; i >= 0; i--) {
+            divisors.add(biggerDivisors.get(i));
+        }
+
+        divisors.add(n);
+        return Converter.listToArr(divisors);
+    }
+    public static long sumOfDivisors(long n) {
+        long[] divisors = divisors(n);
+        long sum = 0;
+
+        for (int i = 0; i < divisors.length-1; i++) {
+            sum += divisors[i];
+        }
+
+        return sum;
+    }
+    public static long greatestCommonDivisor(long a, long b) {
+        if (a < b) {
+            long temp = a;
+            a = b;
+            b = temp;
+        }
+        return greatestCommonDivisorHelper(a, b);
+    }
+    private static long greatestCommonDivisorHelper(long a, long b) {
+        if (b == 0) return a;
+        return greatestCommonDivisorHelper(b, a%b);
+    }
+    public static BigInteger greatestBigCommonDivisor(BigInteger a, BigInteger b) {
+        if (a.compareTo(b) < 0) {
+            BigInteger temp = a;
+            a = b;
+            b = temp;
+        }
+        return greatestBigCommonDivisorHelper(a, b);
+    }
+    private static BigInteger greatestBigCommonDivisorHelper(BigInteger a, BigInteger b) {
+        if (b.equals(BigInteger.ZERO)) return a;
+        return greatestBigCommonDivisorHelper(b, a.remainder(b));
+    }
+}
---
+++ src/main/java/util/Graph.java
@@ -0,0 +1,129 @@
+package util;
+
+import java.util.*;
+
+public class Graph {
+    private final Set<String> nodes = new HashSet<>();
+    private final Set<Edge> edges = new HashSet<>();
+    public Graph() {
+
+    }
+    public Graph(int nodeCount) {
+        for (int i = 0; i < nodeCount; i++) {
+            nodes.add(String.valueOf(i));
+        }
+    }
+    public Graph(int[][] edgeMatrix) {
+        for (int i = 0; i < edgeMatrix.length; i++) {
+            nodes.add(String.valueOf(i));
+        }
+
+        for (int i = 0; i < edgeMatrix.length; i++) {
+            for (int j = 0; j < edgeMatrix[i].length; j++) {
+                if (edgeMatrix[i][j] >= 0) {
+                    edges.add(new Edge(String.valueOf(i), String.valueOf(j), edgeMatrix[i][j]));
+                }
+            }
+        }
+    }
+    public boolean addNode(String node) {
+        return nodes.add(node);
+    }
+    public boolean addEdge(String from, String to, int weight) {
+        if (weight < 0) throw new IllegalArgumentException("Weight: " + weight + " - can't be negative!");
+        if (!nodes.contains(from) || !nodes.contains(to)) return false;
+        return edges.add(new Edge(from, to, weight));
+    }
+    public int nodeCount() {
+        return nodes.size();
+    }
+    Set<Edge> outgoingEdges(String node) {
+        Set<Edge> outgoingEdges = new HashSet<>();
+
+        for (Edge currentEdge : edges) {
+            if (currentEdge.from.equals(node))
+                outgoingEdges.add(currentEdge);
+        }
+
+        return outgoingEdges;
+    }
+    Set<Edge> incomingEdges(String node) {
+        Set<Edge> incomingEdges = new HashSet<>();
+
+        for (Edge currentEdge : edges) {
+            if (currentEdge.to.equals(node))
+                incomingEdges.add(currentEdge);
+        }
+
+        return incomingEdges;
+    }
+
+    public int djikstra(String origin, String destination) {
+        Map<String, Integer> distances = new HashMap<>();
+        for (String node : nodes) {
+            distances.put(node, -1);
+        }
+        distances.put(origin, 0);
+
+        Set<String> visited = new HashSet<>();
+
+        while (visited.size() < nodes.size()) {
+            String node = nextClosestNode(distances, visited);
+            if (node == null) break;
+            if (node.equals(destination))
+                break;
+            visited.add(node);
+            updateDistances(distances, visited, node);
+        }
+
+        return distances.get(destination);
+    }
+
+    private String nextClosestNode(Map<String, Integer> distances, Set<String> visited) {
+        String closestNode = null;
+
+        for (String node : distances.keySet()) {
+            if (distances.get(node) < 0) continue;
+            if (visited.contains(node)) continue;
+
+            if (closestNode == null || distances.get(closestNode) > distances.get(node))
+                closestNode = node;
+        }
+
+        return closestNode;
+    }
+
+    private void updateDistances(Map<String, Integer> distances, Set<String> visited, String node) {
+        int distance = distances.get(node);
+        Set<Edge> edges = outgoingEdges(node);
+
+        for (Edge edge : edges) {
+            if (visited.contains(edge.to)) continue;
+
+            int neighborDistance = distance + edge.weight;
+            if (distances.get(edge.to) == -1 || distances.get(edge.to) > neighborDistance)
+                distances.put(edge.to, neighborDistance);
+        }
+    }
+}
+
+class Edge {
+    String  from;
+    String  to;
+    int weight;
+    public Edge(String  from, String  to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Edge)) return false;
+        return this.equals((Edge) o);
+    }
+
+    public boolean equals(Edge edge) {
+        return from.equals(edge.from) && to.equals(edge.to) && weight == edge.weight;
+    }
+}
